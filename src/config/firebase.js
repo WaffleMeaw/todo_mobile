f28d6adb19c2e49ff69f9ddf909a9cc744502170
Itsara.n <itsara.n@dotography.com>
2017-07-01 15:36:08 +0700

    Config firebase for dev.

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,9 +1,11 @@
 import * as firebase from 'firebase'
 
 export const config = {
-  apiKey: "<API_KEY>",
-  authDomain: "<PROJECT_ID>.firebaseapp.com",
-  databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
-  storageBucket: "<BUCKET>.appspot.com",
+  apiKey: "AIzaSyDjSRkE_hL3l7poe4e7mQoQO5UzKXC2OOA",
+  authDomain: "todo-mobile-1dc52.firebaseapp.com",
+  databaseURL: "https://todo-mobile-1dc52.firebaseio.com",
+  projectId: "todo-mobile-1dc52",
+  storageBucket: "todo-mobile-1dc52.appspot.com",
+  messagingSenderId: "489710870971"
 }
 
