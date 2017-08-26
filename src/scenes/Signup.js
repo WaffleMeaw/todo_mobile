d8bd58062f744095462f73f91f648f939c37154f
Itsara.n <itsara.n@dotography.com>
2017-08-26 16:59:49 +0700

    Edit Signup design page

--- src/scenes/Signup.js
+++ src/scenes/Signup.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
-import { MKTextField, MKButton } from 'react-native-material-kit'
+import { View, Text, TouchableOpacity, TouchableWithoutFeedback, Keyboard, StyleSheet } from 'react-native'
+import { Button, Form, Item, Label, Input } from 'native-base'
 import * as firebase from 'firebase'
 import { NavigationActions } from 'react-navigation'
 
@@ -47,63 +47,35 @@ class Signup extends Component {
     const { navigate, goBack } = this.props.navigation
 
     return (
-      <View style={styles.container}>
-        <Text style={styles.title}>Sign up</Text>
-        <MKTextField
-          value={this.state.email}
-          onChangeText={(text) => this.setState({ email: text })}
-          autoCapitalize={'none'}
-          keyboardType={'email-address'}
-          tintColor={'#ff9010'}
-          highlightColor={'#ed2a2a'}
-          textInputStyle={{ color: '#4c4c4c', fontSize: 20, height: 40, marginBottom: -5 }}
-          placeholder={'Email'}
-          style={styles.textfield}
-          floatingLabelEnabled={true}
-          opacityAniDur={20}
-        />
-        <MKTextField
-          value={this.state.password}
-          onChangeText={(text) => this.setState({ password: text })}
-          autoCapitalize={'none'}
-          tintColor={'#ff9010'}
-          highlightColor={'#ed2a2a'}
-          textInputStyle={{ color: '#4c4c4c', fontSize: 20, height: 40, marginBottom: -5 }}
-          placeholder={'Password'}
-          style={styles.textfield}
-          floatingLabelEnabled={true}
-          opacityAniDur={20}
-        />
-        <MKTextField
-          value={this.state.confirmPassword}
-          onChangeText={(text) => this.setState({ confirmPassword: text })}
-          autoCapitalize={'none'}
-          tintColor={'#ff9010'}
-          highlightColor={'#ed2a2a'}
-          textInputStyle={{ color: '#4c4c4c', fontSize: 20, height: 40, marginBottom: -5 }}
-          placeholder={'Confirm Password'}
-          style={styles.textfield}
-          floatingLabelEnabled={true}
-          opacityAniDur={20}
-        />
-        <MKButton
-          backgroundColor={'#ff9814'}
-          shadowRadius={2}
-          shadowOffset={{ width: 2, height: 2 }}
-          shadowOpacity={0.5}
-          shadowColor="black"
-          style={{ width: 100, height: 40, justifyContent: 'center', alignSelf: 'center', marginBottom: 30 }}
-          onPress={() => this.signup()}
-        >
-          <Text pointerEvents="none"
-            style={{ color: '#4c4c4c', fontSize: 20, textAlign: 'center' }}>
-            Sign up
-          </Text>
-        </MKButton>
-        <TouchableOpacity style={{ alignSelf: 'center' }} onPress={() => goBack()}>
-          <Text>Go to Sign in</Text>
-        </TouchableOpacity>
-      </View>
+      <TouchableWithoutFeedback style={styles.container} onPress={() => Keyboard.dismiss()}>
+        <View>
+          <Text style={styles.title}>Sign up</Text>
+
+          <Form style={{ marginTop: 30, marginBottom: 50, marginRight: 15 }}>
+            <Item floatingLabel>
+              <Label>Username</Label>
+              <Input keyboardType={'email-address'} value={this.state.email} onChangeText={(text) => this.setState({ email: text })} />
+            </Item>
+            <Item floatingLabel>
+              <Label>Password</Label>
+              <Input secureTextEntry value={this.state.password} onChangeText={(text) => this.setState({ password: text })} />
+            </Item>
+            <Item floatingLabel>
+              <Label>Confirm Password</Label>
+              <Input secureTextEntry value={this.state.confirmPassword} onChangeText={(text) => this.setState({ confirmPassword: text })} />
+            </Item>
+          </Form>
+
+          <Button block style={{ marginHorizontal: 15 }} onPress={() => this.signup()}>
+            <Text pointerEvents="none" style={{ color: 'white', fontSize: 20, textAlign: 'center' }}>Register Now</Text>
+          </Button>
+
+          <TouchableOpacity style={{ marginTop: 50, alignSelf: 'center' }} onPress={() => goBack()}>
+            <Text style={{ fontSize: 18 }}>Go to Sign in</Text>
+          </TouchableOpacity>
+
+        </View>
+      </TouchableWithoutFeedback>
     )
   }
 }
@@ -112,7 +84,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 20,
-    backgroundColor: '#ffd484',
   },
   title: {
     marginTop: 50,
